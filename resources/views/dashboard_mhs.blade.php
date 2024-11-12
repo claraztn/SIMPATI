1d209614b5c9ed3a77d02a2f7fda69e20cf25c20
claraztn <claraztn@example.com>
2024-11-13 00:05:00 +0700

    nambahin mhs

--- resources/views/dashboard_mhs.blade.php
+++ resources/views/dashboard_mhs.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Dashboard Bagian Akademik</title>
+    <title>Dashboard Mahasiswa</title>
     <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <style>
         .custom-navbar {
